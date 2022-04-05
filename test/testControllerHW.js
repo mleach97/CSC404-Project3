0895a809dae019407706b4f090cd293ebd18056c
mleach97 <98606574+mleach97@users.noreply.github.com>
2022-04-05 00:19:15 -0400

    Basic mocha tests added

---
+++ test/testControllerHW.js
@@ -0,0 +1,25 @@
+'use strict';
+const { computeHW } = require('../controllers/controller');
+
+var assert = require('assert');
+
+//console.log(computeHW(100, 100, 100));
+
+describe('Test Suite 1 - test computeHW',  ()=> {
+    it('should pass this test', function (){
+        assert.equal(computeHW(100, 100, 100), 20, 'It should be equal to 20');
+    });
+});
+
+describe('Test Suite 2 - test computeHW',  ()=> {
+    it('should pass this test', function (){
+        assert.equal(computeHW(0, 0, 0), 0, 'It should be equal to 0');
+    });
+});
+
+describe.skip('Test Suite 3 - test computeHW',  ()=> {
+    it('should fail this test', function (){
+        assert.equal(computeHW(-100, 100, 100), 20, 'It should be equal to 20');
+    });
+});
+
