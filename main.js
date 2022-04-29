a2590903f1b9200b657235c9d62ea630647a414e
mleach97 <98606574+mleach97@users.noreply.github.com>
2022-04-29 11:36:15 -0400

    Added DB stuff

--- main.js
+++ main.js
@@ -1,34 +1,49 @@
-'use strict';
+  'use strict';
 
-const express = require( 'express' ),
-  layouts = require( 'express-ejs-layouts' ),
-  app = express(),
-  testController = require( './controllers/testController' ),
-  errorController = require( './controllers/errorController' );
+  const express = require( 'express' );
+  const layouts = require( 'express-ejs-layouts' );
+  const app = express();
+  const testController = require( './controllers/testController' );
+  const errorController = require( './controllers/errorController' );
+  const gradesController = require( './controllers/gradesController' );
 
-app.use(
-    express.urlencoded({
-        extended: false
-        })
-    );
-app.use(express.json());
-app.set( 'port', process.env.PORT || 3000 );
+  const { grades } = require( './controllers/testController' );
+  const { computeHW, computeExams, getFinalGrade, getFinalLetterGrade } = require("./controllers/controller");
+  const mongoose = require( 'mongoose' ),
+    Grade = require( './models/grade' );
+    mongoose.Promise = global.Promise;
+    mongoose.connect( 'mongodb://localhost/grades' );
+    const db = mongoose.connection;
 
-app.set( 'view engine', 'ejs' );
-app.use( layouts );
-app.use( express.static( 'public' ) );
+    db.once( 'open', () => {
+      console.log( 'Successfully connected to MongoDB using Mongoose!' );
+    } );
 
-app.get( '/', function( req, res )  {
-  res.render( 'index' );
-} );
+  app.use(
+      express.urlencoded({
+          extended: false
+          })
+      );
+  app.use(express.json());
+  app.set( 'port', process.env.PORT || 3000 );
 
-app.get( '/displayTests', testController.showTestCases );
-app.get( '/addTest', testController.showTCForm );
-app.post( '/addTest', testController.addTestCase );
+  app.set( 'view engine', 'ejs' );
+  app.use( layouts );
+  app.use( express.static( 'public' ) );
 
-app.use( errorController.pageNotFoundError );
-app.use( errorController.internalServerError );
+  app.get( '/', function( req, res )  {
+    res.render( 'index' );
+  } );
 
-app.listen( app.get( 'port' ), () => {
-  console.log( `Server running at http://localhost:${app.get('port')}` );
-} );
+  app.get( '/grades', gradesController.getAllGrades );
+  app.get( '/addTest', gradesController.getGradePage );
+  app.post( '/grade', gradesController.saveGrades );
+
+  app.get( '/displayTests', testController.showTestCases );
+
+  app.use( errorController.pageNotFoundError );
+  app.use( errorController.internalServerError );
+
+  app.listen( app.get( 'port' ), () => {
+    console.log( `Server running at http://localhost:${app.get('port')}` );
+  } );
